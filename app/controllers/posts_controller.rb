8e16d30b2f3152411f1041d6af8c27e6816d612c
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-02 04:40:04 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,6 @@ class PostsController < ApplicationController
     post.title = params[:title]
     post.description = params[:description]
     post.save
-    redirect_to posts_path(@posts)
+    redirect_to post_path(@posts)
   end
 end
